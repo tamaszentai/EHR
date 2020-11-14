35c7d02b481c8a2dcf0c743973b3c9d0f7493d9a
Tamas Zentai <zentaitamas@gmail.com>
2020-11-14 13:46:55 +0000

    hardcoded orderTypes add to orderTypeControllers

---
+++ routes/orderTypeRoutes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const orderTypeControllers = require('../controllers/orderTypeControllers');
+
+const router = express.Router();
+
+router.get('/', orderTypeControllers.getAllOrderTypes);
+
+
+module.exports = router;
