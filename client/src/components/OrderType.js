a7a6d2f08c7926896204838e1046dde5f7e18771
Tamas Zentai <zentaitamas@gmail.com>
2020-11-16 18:47:35 +0000

    delete from orderTypes working from frontend, state refreshes

---
+++ client/src/components/OrderType.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+// import { Table } from "reactstrap";
+
+const OrderType = (props) => {
+
+  const deleteOrderTypeHandler = () => {
+    props.deleteOrderTypeHandler(props.order_code);
+  }
+
+  return (
+    <div className="OrderType">
+          <td style={{width: "5%"}}><strong>Code: </strong>{props.order_code}</td>
+          <td style={{width: "50%"}}><strong>Name: </strong> <br />{props.order_name}</td>
+          <td style={{width: "5%"}}><strong>Specimen: </strong>{props.specimen_type}</td>
+          <td style={{width: "20%"}}><button onClick={deleteOrderTypeHandler}>DELETE</button></td>
+    </div>
+  );
+};
+
+export default OrderType;
--- client/src/containers/OrderTypes.js
+++ client/src/containers/OrderTypes.js
@@ -1,5 +1,9 @@
-import React, {useState, useEffect} from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+import OrderType from "../components/OrderType";
+
+import { Table } from "reactstrap";
 
 const OrderTypes = () => {
   const [orderTypes, setOrderTypes] = useState([]);
@@ -10,12 +14,39 @@ const OrderTypes = () => {
     });
   }, []);
 
+  const deleteOrderTypeHandler = (order_code) => {
+    axios.delete(`http://localhost:5000/api/ordertypes/${order_code}`);
+    let newState = [...orderTypes];
+    newState = orderTypes.filter((data) => data.order_code !== order_code);
+    setOrderTypes(newState);
+  };
+
+  let orderType = null;
+  if (orderTypes !== null) {
+    orderType = orderTypes.map((data, index) => {
+      return (
+        <OrderType
+          key={index}
+          order_code={data.order_code}
+          order_name={data.order_name}
+          specimen_type={data.specimen_type}
+          deleteOrderTypeHandler={deleteOrderTypeHandler}
+        ></OrderType>
+      );
+    });
+  }
+
 
   return (
     <div className="OrderTypes">
       <h1>Order Types</h1>
+      <br />
+      <Table bordered>
+      
+        {orderType}
+      </Table>
     </div>
-  )
-}
+  );
+};
 
 export default OrderTypes;
